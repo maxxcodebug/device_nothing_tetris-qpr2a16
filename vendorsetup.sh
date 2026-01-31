d27b42df3921b759e33db6d5e9e8512c8e40f7b6
SuperAviation001 <superaviation001@gmail.com>
2026-01-31 14:01:18 +1100

    Tetris: clone lineage-23.2 branch of hardware/mediatek

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ -d "hardware/mediatek/aidl/power-mediatek" ]; then
   rm -rf hardware/mediatek
-  git clone https://github.com/SuperAviation001/android_hardware_mediatek -b lineage-22.2 hardware/mediatek
+  git clone https://github.com/SuperAviation001/android_hardware_mediatek -b lineage-23.2 hardware/mediatek
 fi
 if [ ! -d "hardware/mediatek" ]; then
-  git clone https://github.com/SuperAviation001/android_hardware_mediatek -b lineage-22.2 hardware/mediatek
+  git clone https://github.com/SuperAviation001/android_hardware_mediatek -b lineage-23.2 hardware/mediatek
 fi
 if [ ! -d "vendor/nothing/Tetris" ]; then
   git clone https://gitlab.com/abdullashakkeeb693/tetris_vendor_tree -b a15 vendor/nothing/Tetris
