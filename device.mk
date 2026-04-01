8288ca255c3160bc85cd7b133c6889919c76a2cc
abdulla-li <abdullashakkeeb693@gmail.com>
2026-04-01 22:34:33 +0530

    Tetris: Overlay: Move activity open/close animation to its respective dir

--- device.mk
+++ device.mk
@@ -144,7 +144,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    FrameworksResOverlayTetris \
+    FrameworkResOverlayTetris \
     LauncherOverlayTetris \
     SettingsResTetris \
     SystemUIOverlayTetris \
