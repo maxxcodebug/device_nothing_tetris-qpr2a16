7fad63c59e32c4428523bb28bb87f55cb1062663
abdulla-li <abdullashakkeeb693@gmail.com>
2026-04-05 11:56:26 +0530

    Tetris: Remove  IMS debug overrides

--- vendor_props.mk
+++ vendor_props.mk
@@ -417,9 +417,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1 \
     persist.radio.multisim.config=dsds \
     persist.vendor.factory.GB2312=yes \
     persist.vendor.ims_support=1 \
